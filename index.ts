04a384d47d69357d5e439bacd59d7e3820ca33be
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-25 21:02:29 +0300

    fix

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import { JumpFm } from 'jumpfm-api'
 
-export const load = (jumpFm: JumpFm) => {
-    jumpFm.statusBar.info('version', {
-        txt: jumpFm.package.version,
-        dataTitle: 'JumpFm Version'
-    })
-}
+export const load = (jumpFm: JumpFm) =>
+    jumpFm.statusBar.msg('version')
+        .setType('info')
+        .setText(jumpFm.package.version)
+        .setTooltip('JumpFm Version')
